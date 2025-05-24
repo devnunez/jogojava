6a80a04fd33db41afb5d94e9ae3a634dd5651c8d
devnunez <devbrayannunez@gmail.com>
2025-05-24 15:56:37 -0300

    Update Teste.java

--- 03ProjetoJogo/src/pkg03projetojogo/Teste.java
+++ 03ProjetoJogo/src/pkg03projetojogo/Teste.java
@@ -16,6 +16,8 @@ public class Teste {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("teste numero 3");
+        //teste de mensagem
+        
     }
     
 }
